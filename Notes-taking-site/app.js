b70666620196b713e2039bf4cdb5963bdf3c4a2f
Apurba Paul <48132427+apurbapaul1998@users.noreply.github.com>
2021-09-26 22:12:42 +0530

    Update app.js

--- Notes-taking-site/app.js
+++ Notes-taking-site/app.js
@@ -1,16 +1,46 @@
+
+showNotes(); //calling this function because we want to show notes after refreshing the page
 //if user adds a note, add it to the localStorage
-let addBtn=document.getElementById("addBtn");
-addBtn.addEventListener("click", function(e){
-    let addTxt=document.getElementById("addTxt");
-    let notes=localStorage.getItem("notes");
-    if(notes==null){
-        notesObj=[]; //an array
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function (e) {
+    let addTxt = document.getElementById("addTxt");
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = []; //an array
     }
-    else{
-        notesObj=JSON.parse(notes);
+    else {
+        notesObj = JSON.parse(notes);
     }
     notesObj.push(addTxt.value);
-    localStorage.setItem("notes",JSON.stringify(notes));
-    addTxt.value=""; //to make the text area empty upon pushing to the notes section
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value = ""; //to make the text area empty upon pushing to the notes section
     console.log(notesObj);
+    showNotes();
 })
+function showNotes() {
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = []; //an array
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    let html = "";
+    notesObj.forEach(function (element, index) {    //appending the notes in the card
+        html += `<div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">Note ${index + 1}</h5>
+            <p class="card-text">${element}</p>
+            <button href="#" class="btn btn-primary">Delete Node</button>
+        </div>
+    </div>`;
+    });
+    let notesElm = document.getElementById('notes');
+    if (notesObj.length != 0) {
+        notesElm.innerHTML = html;
+    }
+    else{
+        notesElm.innerHTML=`Nothing to show,use 'add a note' section above to show`;
+    }
+
+}
